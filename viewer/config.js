69a76acfed3dab555ad8d311332bbadf69f3cf1c
ozawa hiroshi <hiroshi.ozawa@systems.tocca-net.jp>
2019-07-10 19:54:20 +0900

    initial commit

---
+++ viewer/config.js
@@ -0,0 +1,19 @@
+var linkConfig = [
+
+{
+link: 'demo/sampledemo/sample.html',
+linkName: 'サンプルデモ',
+headerContent: '<h3>これはサンプルです</h3>'
+}
+/*
+,
+
+{
+link: 'demo/demo001/index.html',
+linkName: 'ご提案01',
+headerContent: ''
+}
+*/
+
+];
+
